b0ea3098806f7e0d61e41dad6983bd6648e2caae
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2024-02-20 15:21:38 +0100

    Played around with event listeners.

--- script.js
+++ script.js
@@ -15,4 +15,38 @@ function darkMode() {
     var button = document.querySelector('.dark_mode_button');
     element.classList.toggle("dark-mode");
     button.classList.toggle("dark-mode");
-} 
+}
+
+
+
+
+document.addEventListener('keyup', function (event) {
+    const music = document.getElementById('music');
+    const userInput = event.key.toLowerCase();
+
+
+    // Check if user input matches the specific string
+    if (userInput === 'q') { // For example, registering 'c' as the specific string
+        // Change background image
+        const introduction = document.getElementById('introduction')
+        const myPortrait = document.getElementById('portrait')
+        document.body.style.backgroundImage = "url('https://cdn1.epicgames.com/ue/product/Screenshot/Untitled-1-1920x1080-40d871ba3129e359aaab454149e03063.jpg?resize=1&w=1920')";
+        // Play music
+        introduction.textContent = "Hi! I'm Evil Sean";
+        myPortrait.src = "./resources/easter egg/evil_sean.jpg";
+
+        music.play();
+    } else if (userInput === 'r') { // Assuming 'r' is the key to revert changes
+        // Revert background image
+        document.body.style.backgroundImage = "none";
+        // Revert portrait image source
+        const myPortrait = document.getElementById('portrait');
+        myPortrait.src = "resources/index/my-portrait.jpg"; // Provide the original image source here
+        // Pause music
+        introduction.textContent = "Hi! I'm Sean";
+        music.pause();
+        music.currentTime = 0; // Reset music playback to start
+    }
+
+});
+
